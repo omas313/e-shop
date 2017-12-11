cf5aaae028f72035210c955fe75b1a17b181ca2c
Omar Salam <opace88@gmail.com>
2017-12-11 07:18:43 +0400

    Add order success page

--- src/app/components/order-successful/order-successful.component.ts
+++ src/app/components/order-successful/order-successful.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-successful',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSuccessfulComponent implements OnInit {
 
-  constructor() { }
+  orderId: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.orderId = this.route.snapshot.paramMap.get("id");
   }
 
 }
